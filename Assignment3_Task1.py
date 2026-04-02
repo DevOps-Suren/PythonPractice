e813a4711f8719b8fdb8eec4846714103175a1bf
DevOps-Suren <aws.surend@gmail.com>
2026-04-02 19:14:30 +0530

    Assignment 3_Task1

---
+++ Assignment3_Task1.py
@@ -0,0 +1,19 @@
+"""
+Assignment 3 - Task 1 -  Calculate Factorial Using a Function
+
+Problem Statement: Write a Python program that:
+1.   Defines a function named factorial that takes a number as an argument and calculates its factorial using a loop or recursion.
+2.   Returns the calculated factorial.
+3.   Calls the function with a sample number and prints the output.
+
+"""
+
+def factorial(num):
+    factorial =1
+    while num > 1:
+        factorial *= num
+        num -= 1
+    return factorial
+
+n =int(input("Enter a Number:"))
+print(f"Factorial for {n} is {factorial(n)}")
