8b18c5561e380afe0d93db207c92407d651729d9
DevOps-Suren <aws.surend@gmail.com>
2026-06-18 11:56:12 +0530

    Assignment5_Task1

---
+++ Assignments_5/Assignment5_Task1.py
@@ -0,0 +1,13 @@
+student1 ={"name":"John","Marks":[65,55,85]}
+student2 ={"name":"Brad","Marks":[70,80,65]}
+student3 ={"name":"David","Marks":[85,75,95]}
+
+StudentDetails =str(input("Enter the Student Name: "))
+if StudentDetails == student1["name"]:
+    print(f"Marks of  {StudentDetails} are {student1["Marks"]}")
+elif StudentDetails == student2["name"]:
+        print(f"Marks of  {StudentDetails} are {student2["Marks"]}")
+elif StudentDetails == student3["name"]:
+            print(f"Marks of  {StudentDetails} are {student3["Marks"]}")
+else:
+    print("Student not found")
