3c262fafff7dad0e55167e4eb8710e2c6d3d7006
DevOps-Suren <aws.surend@gmail.com>
2026-06-19 09:54:30 +0530

    Assignment5_Task1

---
+++ Assignments_5/Assignment5_Task2.py
@@ -0,0 +1,10 @@
+Original_list =(1,2,3,4,5,6,7,8,9,10)
+print(f"Original List : {Original_list}")
+
+numbers = list(range(1,11))
+extracted =numbers[:5]
+
+reversed_list = extracted[::-1]
+
+print(f"Extracted List : {extracted}")
+print(f"Reversed_List List : {reversed_list}")
